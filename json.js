de43079e53efc5819c6b7b2364c3bb20b9a79e99
Robbitmm <46769452+Robbitmm@users.noreply.github.com>
2020-11-21 01:56:28 -0200

    Preview

---
+++ json.js
@@ -0,0 +1,63 @@
+﻿var armarios = [],
+
+armarios = [
+    {
+        "numero": 2,
+        "prateleira": [1, 2, 3, 4],
+        "itens": [
+                    ["Chave combinada (935405) 6 - 11 mm"],
+                    ["Chave combinada (935405) 12 - 32 mm"],
+                    ["Chave de boca (885656) 6 - 32 mm"],
+                    ["Chave de boca (885656) 6 - 28 mm"]
+                ]
+    },
+    {
+        "numero": 3,
+        "prateleira": [1, 2, 3, 4],
+        "itens": [ 
+                    [ //Prateleira 1
+                     'Chave de fenda cruzada 5/16"x6" (2x)', 
+                     'Chave de fenda cruzada 3/16"x5" (2x)',
+                     'Chave de fenda cruzada 1/8"4" (2x)',
+                     'Chave de fenda 1/8"x4" (2x)',
+                     'Chave de fenda 3/16" (2x)',
+                     'Chave de fenda 3/8"x10" (2x)',
+                     'Chave de fenda 1/4"x6" (3x)',
+                     'Martelo bola (2x)',
+                     'Martelo borracha (1x)',
+                     'Marreta (1x)',
+                     'Chave Allen 0,55 - 12 mm',
+                     'Chave Allen 1/16" - 1 1/2"',
+                     'Talhadeira 120mm',
+                     'Punsão (2x)',
+                     'Saca pino Ø4,15 - Ø6 (3x)'
+                    ],
+                    [ //Prateleira 2
+                     'Chave catraca (soquetes de 9 - 24 mm)',
+                     'Junta universal (2x)',
+                     'Extensor (2x)',
+                     'Grifo 250mm (885660) (2x)',
+                     'Chave inglesa 250mm (1126128) (2x)',
+                     'Alicate (905459 e 905458) (2x)',
+                     'Alicate anel de retenção de bico curvo (1x)',
+                     'Alicate anel de retenção de bico reto (1x)',
+                     'Cabo de força (2x)'
+                    ],
+                    [ //Prateleira 3
+                     'Lima bastarda',
+                     'Lima grosa (1x)',
+                     'Alicates universais (885620 e 885621) (2x)',
+                     'Alicate de pressão (3x)',
+                     'Extrator de prisioneira (2x)',
+                     'Chave combinada (906688) 1/4" - 1 1/4"'
+                    ],
+                    [ //Prateleira 4
+                     'Chave de boca (988447) 1/4" - 1 1/2"',
+                     'Lâmina de cerra (2x)'
+                    ]
+                ]
+    }
+];
+
+//armarios.push(G);
+ 
---
+++ writer.js
@@ -0,0 +1,56 @@
+﻿function Writing(){
+    let count = 0;
+    
+    for(var a in G){
+        console.log("Armário " + G[count].numero);
+
+        for(c in G[count].itens){
+            console.log(G[count].itens[c]);
+            it = G[count].itens[c];
+
+	    p = document.createElement('p');
+            p.innerHTML += it
+        }
+        count ++;
+    }
+}
+
+function Writting2(){
+    for(let a = 0; a < armarios.length; a++){
+        const newH1 = document.createElement('h1'),
+              oldDiv = document.querySelector("div");
+
+        let armariosN = armarios[a];
+
+        oldDiv.append(newH1);
+        newH1.innerHTML = "Armário " + armariosN.numero;
+
+        for(let p = 0; p < armariosN.prateleira.length; p++){
+            const Ul = document.createElement('ul'),
+                  Li = document.createElement('li');
+
+            let prateleiraN = armariosN.prateleira[p];
+
+            Li.classList.add('prateleira');
+
+            oldDiv.append(Ul);
+            Ul.append(Li);
+
+            Li.innerHTML = "Prateleira " + prateleiraN;
+
+            for(i = 0; i < armariosN.itens.length; i++){
+                let item = armariosN.itens[i];
+
+                for(v = 0; v < item.length; v++){
+                    const Ul2 = document.createElement('ul'),
+                          Li2 = document.createElement('li');
+    
+                    Li.append(Ul2);
+                    Ul2.append(Li2);
+    
+                    Li2.innerHTML = item[v];
+                }
+            }
+        }
+    }
+}
